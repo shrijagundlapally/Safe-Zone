97a7d3198a708744d1c42138c06d50e21f9275c1
Amy Ding <amyding04@gmail.com>
2022-07-20 14:23:01 -0500

    Initial Commit

---
+++ Safe Zone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Safe Zone
+//
+//  Created by Amy Ding on 2022-07-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
