71572edfaecdd235cc684f99dfdd817d35c48e59
Fateha Choudhury <fatehachouydhury06@gmail.com>
2022-07-20 23:51:21 -0500

    I added button and edited the hotline

--- Safe Zone/hotlinesViewController.swift
+++ Safe Zone/hotlinesViewController.swift
@@ -9,13 +9,40 @@ import UIKit
 
 class hotlinesViewController: UIViewController {
 
+    @IBOutlet weak var firstPic: UIImageView!
+    @IBOutlet weak var secondPic: UIImageView!
+    @IBOutlet weak var thirdPic: UIImageView!
+    @IBOutlet weak var fourthPic: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        firstPic.isHidden = true
+        secondPic.isHidden = true
+        thirdPic.isHidden = true
+        fourthPic.isHidden = true
     }
     
 
+    @IBAction func firstButton(_ sender: Any) {
+        firstPic.isHidden = false
+    }
+    
+    @IBAction func secondButton(_ sender: Any) {
+        secondPic.isHidden = false
+    }
+    
+    
+    
+    @IBAction func thirdButton(_ sender: Any) {
+        thirdPic.isHidden = false
+    }
+    
+    
+    @IBAction func fourthButton(_ sender: Any) {
+        fourthPic.isHidden = false
+    }
+    
     /*
     // MARK: - Navigation
 
